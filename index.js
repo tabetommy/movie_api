f8a417ff082a9271926cde5b0e237dad528de4c9
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-01 11:13:21 +0200

    imported express package and declared a favouriteMovies variable

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
+const express=require('express');
+const app=express();
+const morgan=require('morgan');
+
+
+const favouriteMovies=[
+    {
+        title:'Game of Thrones',
+        year:2011,
+        category:'Adventure'
+
+    },
+    {
+        title:'Abejoye',
+        year:2018,
+        category:'Gospel'
+    },
+    {
+        title:'War Room',
+        year:2015,
+        category:'Gospel'
+    },
+    {
+        title:'Blacklist',
+        year:2013,
+        category:'Crime'
+    },
+    {
+        title:'Legend of the Seeker',
+        year:2008,
+        category:'Adventure'
+    },
+    {
+        title:'King of the Boys',
+        year:2018,
+        category:'Political Thriller'
+    },
+    {
+        title:'24',
+        year:2001,
+        category:'Crime thriller'
+    },
+    {
+        title:'House of Cards',
+        year:2013,
+        category:'Political Drama'
+    },
+    {
+        title:'Power',
+        year:2014,
+        category:'Crime Drama'
+    },
+    {
+        title:'THe Weddding Party 1',
+        year:2016,
+        category:'Comedy'
+    },
+];
+
+
+
+
 
 
 
