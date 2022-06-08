5ff75ca9cff63eba0067079530d6821917c73c2e
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-08 11:11:54 +0200

    added function to get all movies

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const express=require('express');
       morgan=require('morgan');
       bodyParser = require('body-parser'),
       uuid = require('uuid');
+      mongoose= require('mongoose');
+      Models= require('./models.js');
+      Movies= Models.Movie;
+      Users= Models.User;
       
 
 app.use(bodyParser.json());
-
-
-
+mongoose.connect('mongodb://localhost:27017/myFlix', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(morgan('common'));
 
-
+//return list of all movies to client(ok)
+app.get('/movies',(req,res)=>{
+  Movies.find()
+  .then(movies=>res.json(movies))
+  .catch(error=>{
+    console.error(error);
+    res.status(500).send('Error: ' + error)
+  })
+});
 
 app.use(express.static('public'));
 
