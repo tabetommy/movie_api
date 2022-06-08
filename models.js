50414d0fa65eb1adc00722789bfe4cb743b311ba
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-08 11:03:13 +0200

    Declared the user and movie schemas with mongoose

---
+++ models.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+let movieSchema=mongoose.Schema({
+    Title:{type:String, required:true},
+    Description:{type:String, required:true},
+    Genre:{
+        Name:String, 
+        Description:String
+        },
+    Director:{
+        Name:String,
+        Bio:String,
+        Birth:Date,
+        Death:Date
+    },
+    ImagePath:String,
+    Featured:Boolean,
+});
+
+let userSchema=mongoose.Schema({
+    Username:{type: String, required:true},
+    Password:{type: String, required:true},
+    Email:{type: String, required:true},
+    Birthdate:Date,
+    FavouritesMovies:[{type:mongoose.Schema.Types.ObjectId, ref:'Movie'}]
+});
