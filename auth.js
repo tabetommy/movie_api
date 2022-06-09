12fe514b5cf4580e0bba504d100fa12eff6b3ab2
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-09 19:17:29 +0200

    created login endpoint for already registered users in auth.js file

---
+++ auth.js
@@ -0,0 +1,34 @@
+const jwtSecret='my_jwt_secret';// same key used in strategy
+const jwt=require('jsonwebtoken'),
+      passport=require('passport');
+
+require('./passport');
+
+let generateJWTToken= (user)=>{
+    return jwt.sign(user, jwtSecret,{
+        subject:user.Username,
+        expiresIn: '7d',
+        algorithm:'HS256'
+    })
+}
+
+/* Post login*/
+module.exports=(router)=>{
+    router.post('/login', (req,res)=>{
+      passport.authenticate('local',{session:false},(error,user,info)=>{
+          if(error || !user){
+              return res.status(400).json({
+                  message:'Something is not right',
+                  user:user
+              })
+          }
+          req.login(user,{session: false}, (error)=>{
+              if(error){
+                  res.send(error);
+              }
+              let token = generateJWTToken(user.toJSON());
+              return res.json({user,token});
+          });
+      })(req,res)
+    })
+}
--- passport.js
+++ passport.js
@@ -30,7 +30,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
     jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey:'your_jwt_secret'
+    secretOrKey:'my_jwt_secret'
 },(jwtPayload, callback)=>{
     return Users.findById(jwtPayload._id)
     .then(user=>{
@@ -39,4 +39,4 @@ passport.use(new JWTStrategy({
     .catch(error=>{
         return callback(error)
     });
-}))
+}));
