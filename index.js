d8cacff94e71036ac1cd837920882cafe7429d74
Tommy Egbe <tabe_e@yahoo.ca>
2022-07-16 12:43:54 +0200

    deleted date validation to update user info endpoint

--- index.js
+++ index.js
@@ -119,8 +119,7 @@ app.put('/users/:username',[
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail(),
-  check('Birthday', 'Birthday does not appear to be valid').isDate()
+  check('Email', 'Email does not appear to be valid').isEmail()
 ],passport.authenticate('jwt', { session: false }),(req,res)=>{
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
