72037c5c91d94f4a055ba8193dd03c58380297ec
Tommy Egbe <tabe_e@yahoo.ca>
2022-07-25 10:57:15 +0200

    added date to update user endpoint

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.put('/users/:username',[
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to be valid').isEmail(),
-  // check('Birthday', 'Birthday does not appear to be valid').isDate()
+  check('Birthday', 'Birthday does not appear to be valid').isDate()
 ],passport.authenticate('jwt', { session: false }),(req,res)=>{
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
